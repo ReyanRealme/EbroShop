8b48095713712066ab5c7641e0f7df4f752dad1f
Realme <rebyidejene8949@gmail.com>
2026-01-01 23:41:39 +0300

    Add files via upload

--- db.php
+++ db.php
@@ -1,12 +1,14 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "ebroshop";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$port = getenv('DB_PORT');
+$dbname = "defaultdb";
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
 
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if (!mysqli_real_connect($conn, $host, $user, $pass, $dbname, $port)) {
+    die("Connection failed");
 }
 ?>
