ce4a87dc501255de56eb7d594180ec8b8fc0d5fa
Realme Hassen <rebyidejene8949@gmail.com>
2026-01-23 11:11:40 +0300

    Add files via upload

--- languages.js
+++ languages.js
@@ -4,7 +4,7 @@ const ebroTranslations = {
     "en": {
   /*Common for all pages*/   
  /* Overlay Menu*/
-         "common1": "✍️Sign Up",
+         "common1": "Sign Up/Register",
          "common2": "Home",
          "common3": "About-us",
          "common4": "Contact-us",
